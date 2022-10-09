6fe6a3122e03c5d6482da65e0babd813666a4b28
nikhil0162 <nikhil0162@gmail.com>
2022-10-09 23:21:46 +0530

    Adding Pictures to Cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+              {/* div product-info */}
+            </div>
+            // product
+          ))}
+        </div>
+        {/* div products */}
+      </main>
     </div>
   );
 }

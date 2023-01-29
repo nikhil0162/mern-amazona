cc5c2ad07c159eb0cfdebc4219eaa7f1a41ed623
nikhil0162 <nikhil0162@gmail.com>
2023-01-29 23:26:43 +0530

    working on CartScreen

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,51 @@
+import { useContext } from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+import MessageBox from '../components/MessageBox';
+
+import { Store } from '../Store';
+
+export default function CartScreen() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  return (
+    <div>
+      <Helmet>
+        <title>Shopping Cart</title>
+      </Helmet>
+      <h1>Shopping Cart</h1>
+      <Row>
+        <Col md={8}>
+          {cartItems.length === 0 ? (
+            <MessageBox>
+              Cart is empty. <Link to="/">Go Shopping</Link>
+            </MessageBox>
+          ) : (
+            <ListGroup>
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item._id}>
+                  <Row className="align-items-center">
+                    <Col md={4}>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="img-fluid rounded"
+                      />{' '}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
+}
